548eed943fd3d22a9cc1bb5147220e6212e97600
Mike Redrobe <mike@redrobe.net>
2024-05-07 12:30:16 +0100

    Create rj45alive.py

---
+++ rj45alive.py
@@ -0,0 +1,227 @@
+
+from machine import Pin,SPI,PWM
+import framebuf
+import time
+import network, usocket
+
+BL = 13
+DC = 8
+RST = 12
+MOSI = 11
+SCK = 10
+CS = 9
+
+class LCD_1inch3(framebuf.FrameBuffer):
+    def __init__(self):
+        self.width = 240
+        self.height = 240
+        
+        self.cs = Pin(CS,Pin.OUT)
+        self.rst = Pin(RST,Pin.OUT)
+        
+        self.cs(1)
+        self.spi = SPI(1)
+        self.spi = SPI(1,1000_000)
+        self.spi = SPI(1,100000_000,polarity=0, phase=0,sck=Pin(SCK),mosi=Pin(MOSI),miso=None)
+        self.dc = Pin(DC,Pin.OUT)
+        self.dc(1)
+        self.buffer = bytearray(self.height * self.width * 2)
+        super().__init__(self.buffer, self.width, self.height, framebuf.RGB565)
+        self.init_display()
+        
+        self.red   =   0x07E0
+        self.green =   0x001f
+        self.blue  =   0xf800
+        self.white =   0xffff
+        
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
+
+    def init_display(self):
+        """Initialize dispaly"""  
+        self.rst(1)
+        self.rst(0)
+        self.rst(1)
+        
+        self.write_cmd(0x36)
+        self.write_data(0x70)
+
+        self.write_cmd(0x3A) 
+        self.write_data(0x05)
+
+        self.write_cmd(0xB2)
+        self.write_data(0x0C)
+        self.write_data(0x0C)
+        self.write_data(0x00)
+        self.write_data(0x33)
+        self.write_data(0x33)
+
+        self.write_cmd(0xB7)
+        self.write_data(0x35) 
+
+        self.write_cmd(0xBB)
+        self.write_data(0x19)
+
+        self.write_cmd(0xC0)
+        self.write_data(0x2C)
+
+        self.write_cmd(0xC2)
+        self.write_data(0x01)
+
+        self.write_cmd(0xC3)
+        self.write_data(0x12)   
+
+        self.write_cmd(0xC4)
+        self.write_data(0x20)
+
+        self.write_cmd(0xC6)
+        self.write_data(0x0F) 
+
+        self.write_cmd(0xD0)
+        self.write_data(0xA4)
+        self.write_data(0xA1)
+
+        self.write_cmd(0xE0)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0D)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2B)
+        self.write_data(0x3F)
+        self.write_data(0x54)
+        self.write_data(0x4C)
+        self.write_data(0x18)
+        self.write_data(0x0D)
+        self.write_data(0x0B)
+        self.write_data(0x1F)
+        self.write_data(0x23)
+
+        self.write_cmd(0xE1)
+        self.write_data(0xD0)
+        self.write_data(0x04)
+        self.write_data(0x0C)
+        self.write_data(0x11)
+        self.write_data(0x13)
+        self.write_data(0x2C)
+        self.write_data(0x3F)
+        self.write_data(0x44)
+        self.write_data(0x51)
+        self.write_data(0x2F)
+        self.write_data(0x1F)
+        self.write_data(0x1F)
+        self.write_data(0x20)
+        self.write_data(0x23)
+        
+        self.write_cmd(0x21)
+
+        self.write_cmd(0x11)
+
+        self.write_cmd(0x29)
+
+    def show(self):
+        self.write_cmd(0x2A)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xef)
+        
+        self.write_cmd(0x2B)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0x00)
+        self.write_data(0xEF)
+        
+        self.write_cmd(0x2C)
+        
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(self.buffer)
+        self.cs(1)
+        
+ # Check internet connectivity by sending DNS lookup to Google's 8.8.8.8
+def pingdns(ip):
+    packet=b'$\x1a\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03www\x06google\x03com\x00\x00\x01\x00\x01'
+    dns_response_len = 32 # DNS query and response packet size
+    s = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
+    s.settimeout(1)
+    s.connect((ip, 53))
+
+    rec = b''
+    s.write(packet)
+    try:
+        while len(rec) < dns_response_len:
+            res = s.read(dns_response_len - len(rec))
+            if res is not None:
+                rec = b''.join((rec, res))
+        
+        if len(rec) == dns_response_len:
+            #print('google said: {}'.format(rec))
+            return True
+    except OSError: # Timeout on read: no connectivity.
+        return False
+    finally:
+        s.close()
+    return False        
+  
+if __name__=='__main__':
+    pwm = PWM(Pin(BL))
+    pwm.freq(1000)
+    pwm.duty_u16(32768)#max 65535
+
+    LCD = LCD_1inch3()
+    
+    keyA = Pin(15,Pin.IN,Pin.PULL_UP)
+    keyB = Pin(17,Pin.IN,Pin.PULL_UP)
+    keyX = Pin(19 ,Pin.IN,Pin.PULL_UP)
+    keyY= Pin(21 ,Pin.IN,Pin.PULL_UP)
+    
+    up = Pin(2,Pin.IN,Pin.PULL_UP)
+    dowm = Pin(18,Pin.IN,Pin.PULL_UP)
+    left = Pin(16,Pin.IN,Pin.PULL_UP)
+    right = Pin(20,Pin.IN,Pin.PULL_UP)
+    ctrl = Pin(3,Pin.IN,Pin.PULL_UP)
+    
+    
+    spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
+    nic = network.WIZNET5K(spi,Pin(17),Pin(20)) #spi,cs,reset pin
+    nic.active(True)
+    
+    
+    while True:
+        LCD.fill(LCD.blue)    
+        time.sleep(1)
+        if not nic.isconnected():
+            LCD.text("disconnected...",10,30,LCD.white)
+            LCD.show()
+            time.sleep(1)
+        else: # connected
+            ip,netmask,gateway,dns = nic.ifconfig()
+    
+            LCD.text("IP addr: " + ip,10,30,LCD.white)
+    
+            LCD.text("Netmask: " + netmask,10,50,LCD.white)
+            LCD.text("Gateway: " + gateway,10,70,LCD.white)
+            LCD.text("DNS    : " + dns,10,110,LCD.white)
+        
+        
+            online = pingdns('8.8.8.8')
+            if online == True:
+                LCD.text("Internet : ONLINE",10,150,LCD.green)
+            else:
+                LCD.text("Internet : Offline",10,150,LCD.red)
+            LCD.show()
+        
+            time.sleep(3)
